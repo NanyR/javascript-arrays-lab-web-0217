66519cf0e612ed21f8c5afcc5e97729023afe6e9
nanyr <nanyramirez0@gmail.com>
2017-03-16 17:52:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name){
+  window.kittens.push(name);
+
+}
+
+function destructivelyPrependKitten(name){
+  window.kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(){
+  window.kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten(){
+  window.kittens.splice(0, 1);
+}
+
+function appendKitten(name){
+  var newKittens=[...window.kittens, name];
+  return newKittens
+}
+
+function prependKitten(name){
+  var newKittens=[name, ...window.kittens];
+  return newKittens;
+}
+
+function removeLastKitten(){
+  var newKittens= window.kittens.slice(0, window.kittens.length-1);
+  return newKittens
+}
+
+function removeFirstKitten(){
+  var newKittens=window.kittens.slice(1);
+  return newKittens
+}
